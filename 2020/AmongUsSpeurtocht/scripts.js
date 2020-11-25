56fe3ab88bc70825e789481d9e5164ca35a7b139
Jan Willem Boer <j.w.boer@saxion.nl>
2020-11-25 11:45:32 +0100

    externe files

---
+++ 2020/AmongUsSpeurtocht/scripts.js
@@ -0,0 +1,44 @@
+
+const instructionText = document.getElementById("instructiontext");
+let textsToWrite = [];
+let currentP;
+let currentText = "";
+const ps = [];
+
+const tags = instructionText.getElementsByTagName("P");
+for (const p of tags) {
+    textsToWrite.push(p.innerHTML);
+    p.innerHTML = " ";
+    ps.push(p);
+}
+
+setTimeout(addNextChar, 50);
+
+function addNextChar() {
+
+    if (!currentText) {
+        currentText = textsToWrite.shift();
+        if (!currentText) {
+            return;
+        }
+
+        currentP = ps.shift();
+    }
+
+    const curChar = currentText.substr(0, 1);
+    currentText = currentText.substr(1);
+    currentP.innerHTML += curChar;
+
+    let nextTimeOutAt = 50;
+
+    if (!currentText) {
+        nextTimeOutAt = 2000; // delay on new <P>
+    } else {
+        const peekNext = currentText.substr(0, 1);
+        if ((curChar === "." || curChar === "!") && peekNext === " ") {
+            nextTimeOutAt = 1000; // delay on space
+        }
+    }
+
+    setTimeout(addNextChar, nextTimeOutAt);
+}
